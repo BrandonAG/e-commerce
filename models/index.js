33fc5bdde8792b52a4a792928172127fe5a8b8d0
Brandon <brandon.eg@gmail.com>
2021-03-08 00:44:31 -0800

    fixed foreign key issue in models

--- models/index.js
+++ models/index.js
@@ -16,12 +16,14 @@ Category.hasMany(Product, {
 
 // Products belongsToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
-  through: ProductTag
+  through: ProductTag,
+  foreignKey: 'product_id'
 });
 
 // Tags belongsToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
-  through: ProductTag
+  through: ProductTag,
+  foreignKey: 'tag_id'
 });
 
 module.exports = {
